54d446682e47d8c35867ce23ecffe226072c4d30
FanQiang <851524519@qq.com>
2019-09-02 21:24:10 +0800

    '第一次提交'

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/* 
+ * @file config-overrides.js
+ * @author Fan Qiang(851524519@qq.com)
+ * 基于customize和react-app-rewired的定制化配置文件
+*/
+
+// 从customize-cra引入一些相关的方法
+const { override } = require('customize-cra')
+
+module.exports = override()
