26c8ef3c8d3b7af8fce9b1c75a34a7f80790130a
FanQiang <851524519@qq.com>
2019-09-03 11:03:18 +0800

    配置less及antd

--- config-overrides.js
+++ config-overrides.js
@@ -9,11 +9,20 @@
 // 从customize-cra引入一些相关的方法
 const { 
     override,
-    addLessLoader
+    addLessLoader,
+    fixBabelImports
  } = require('customize-cra')
+
+const modifyVars = require('./lessVars')
 
 module.exports = override(
     addLessLoader({
-        javascriptEnabled: true
-    })
+        javascriptEnabled: true,
+        modifyVars
+    }),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
 )
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
+import {
+    Button
+} from 'antd'
+
 export default class App extends Component {
     render() {
         return (
             <div>
                 App
+                <Button type="primary">ANTD</Button>
             </div>
         )
     }
